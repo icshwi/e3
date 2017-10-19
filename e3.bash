ce816fd538a3a91f1f92eb378105e502984150e4
Jeong Han Lee <jeonghan.lee@gmail.com>
2017-10-19 15:17:47 +0200

    test upto make env

--- e3.bash
+++ e3.bash
@@ -1,3 +1,42 @@
+#!/bin/bash
+#
+#  Copyright (c) 2017 - Present European Spallation Source ERIC
+#
+#  The program is free software: you can redistribute
+#  it and/or modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation, either version 2 of the
+#  License, or any newer version.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU General Public License along with
+#  this program. If not, see https://www.gnu.org/licenses/gpl-2.0.txt
+#
+#
+#   author  : Jeong Han Lee
+#   email   : jeonghan.lee@gmail.com
+#   date    : Thursday, October 19 15:16:54 CEST 2017
+#   version : 0.0.1
+
+
+declare -gr SC_SCRIPT="$(realpath "$0")"
+declare -gr SC_SCRIPTNAME=${0##*/}
+declare -gr SC_TOP="$(dirname "$SC_SCRIPT")"
+
+
+ROOT_UID=0 
+E_NOTROOT=101
+EXIST=1
+NON_EXIST=0
+
+function pushd() { builtin pushd "$@" > /dev/null; }
+function popd()  { builtin popd  "$@" > /dev/null; }
+
+
+
 GIT_URL="https://github.com/icshwi"
 GIT_CMD="git clone"
 
@@ -25,9 +64,10 @@ done
 
 
 for rep in  ${module_list[@]}; do
-    cd ${rep}
+    pushd ${rep}
     make init
     make env
+    popd
 done
 
 
