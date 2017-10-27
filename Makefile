eb0da6ef4612c389bb633e6cff08c9c3fa71896f
Jeong Han Lee <jeonghan.lee@gmail.com>
2017-10-27 18:51:22 +0200

    refactor e3 and add makefile

---
+++ Makefile
@@ -0,0 +1,77 @@
+#
+#  Copyright (c) 2017 - Present  European Spallation Source ERIC
+#
+#  The program is free software: you can redistribute
+#  it and/or modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation, either version 2 of the
+#  License, or any newer version.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU General Public License along with
+#  this program. If not, see https://www.gnu.org/licenses/gpl-2.0.txt
+#
+# Author  : Jeong Han Lee
+# email   : han.lee@esss.se
+# Date    : Friday, October 27 15:37:55 CEST 2017
+# version : 0.0.1
+
+TOP:=$(CURDIR)
+
+
+ifndef VERBOSE
+  QUIET := @
+endif
+
+ifdef DEBUG_SHELL
+  SHELL = /bin/sh -x
+endif
+
+
+# help is defined in 
+# https://gist.github.com/rcmachado/af3db315e31383502660
+help:
+	$(info --------------------------------------- )	
+	$(info Available targets)
+	$(info --------------------------------------- )
+	$(QUIET) awk '/^[a-zA-Z\-\_0-9]+:/ {            \
+	  nb = sub( /^## /, "", helpMsg );              \
+	  if(nb == 0) {                                 \
+	    helpMsg = $$0;                              \
+	    nb = sub( /^[^:]*:.* ## /, "", helpMsg );   \
+	  }                                             \
+	  if (nb)                                       \
+	    print  $$1 "\t" helpMsg;                    \
+	}                                               \
+	{ helpMsg = $$0 }'                              \
+	$(MAKEFILE_LIST) | column -ts:	
+
+
+default: help
+
+#
+## Clean all e3-*
+clean:
+	$(QUIET) sudo -E bash -c 'rm -rf e3-*'
+#
+## Build all 
+build:
+	$(QUIET) bash e3.bash all
+
+## Print modules list
+env:
+	$(QUIET) bash e3.bash env
+
+## Setup Base and Require
+base:
+	$(QUIET) bash e3.bash base
+
+## Setup Modules
+modules:
+	$(QUIET) base e3.bash modules
+
+
+.PHONY: help clean build env base modules
--- e3.bash
+++ e3.bash
@@ -21,7 +21,6 @@
 #   date    : Thursday, October 19 17:25:33 CEST 2017
 #   version : 0.0.2
 
-set -e
 
 declare -gr SC_SCRIPT="$(realpath "$0")"
 declare -gr SC_SCRIPTNAME=${0##*/}
@@ -34,45 +33,82 @@ function popd  { builtin popd  "$@" > /dev/null; }
 GIT_URL="https://github.com/icshwi"
 GIT_CMD="git clone"
 
-function git_clone {
+
+declare -g  env="e3-env"
+declare -ga require_list=("e3-base" "e3-require")
+declare -ga module_list=()
+
+
+function help
+{
+    echo "">&2
+    echo " Usage: $0 <arg>  ">&2 
+    echo ""
+    echo "          <arg>   : info">&2 
+    echo ""
+    echo "           base    : Setup and Build Base and Require">&2
+    echo "           modules : Setup and Build all modules">&2
+    echo "           env     : Print all modules list">&2
+    echo "">&2
+    echo "">&2 	
+}
+
+function git_clone
+{
 
     local rep_name=$1
     ${GIT_CMD} ${GIT_URL}/$rep_name
     
 }
 
-declare -g  env="e3-env"
-declare -ga require_list=("e3-base" "e3-require")
-declare -ga module_list=()
 
-module_list+="e3-iocStats";
-module_list+=" " ; module_list+="e3-devlib2";
-#module_list+=" " ; module_list+="e3-mrfioc2";
-
-
-for rep in  ${require_list[@]}; do
-    git_clone ${rep}
-    pushd ${rep}
-    make init
-    make env
-    if [ "${rep}" = "e3-base" ]; then
-	make pkgs
-    fi
-    popd
-done
+function get_module_list
+{
+    local i;
+    let i=0
+    while IFS= read -r line_data; do
+	if [ "$line_data" ]; then
+	    # Skip command #
+	    [[ "$line_data" =~ ^#.*$ ]] && continue
+	    entry[i]="${line_data}"
+	    ((++i))
+	fi
+    done < $1
+    echo ${entry[@]}
+}
 
 
-sudo -v
 
+function setup_base_require
+{
 
-for rep in  ${require_list[@]}; do
-   pushd ${rep}
-   make build
-   if [ "${rep}" = "e3-require" ]; then
-	make install
-    fi
-   popd
-done
+    for rep in  ${require_list[@]}; do
+	git_clone ${rep}
+	pushd ${rep}
+	make init
+	make env
+	if [ "${rep}" = "e3-base" ]; then
+	    make pkgs
+	fi
+	popd
+    done
+}
+
+function build_base_require
+{
+
+    sudo -v
+    
+    
+    for rep in  ${require_list[@]}; do
+	pushd ${rep}
+	make build
+	if [ "${rep}" = "e3-require" ]; then
+	    make install
+	fi
+	popd
+    done
+}
 
 
 #
@@ -80,25 +116,86 @@ done
 # Since we have the global env variables,
 # it should be OK to use them
 #
-git_clone ${env}
-pushd ${env}
-source setE3Env.bash
-popd
-
-
-
-for rep in  ${module_list[@]}; do
-    git_clone ${rep}
-    pushd ${rep}
-    make init
-    make env
+  
+function setup_env
+{
+    git_clone ${env}
+    pushd ${env}
+    source setE3Env.bash
     popd
-done
 
-for rep in  ${module_list[@]}; do
-    pushd ${rep}
-    make build
-    make install
-    popd
-done
+}
+
+
+
+function print_list
+{
+    local array=$1
+    for a_list in ${array[@]}; do
+	printf " %s\n" "$a_list";
+    done
+}
+
+function setup_modules
+{
+    for rep in  ${module_list[@]}; do
+	git_clone ${rep}
+	pushd ${rep}
+	make init
+	make env
+	popd
+    done
+
+}
+
+function build_modules
+{
+
+    for rep in  ${module_list[@]}; do
+	pushd ${rep}
+	make build
+	make install
+	popd
+    done
+}
+
+
+module_list=$(get_module_list ${SC_TOP}/configure/MODULES)
+
+
+#module_list+="e3-iocStats";
+#module_list+=" " ; module_list+="e3-devlib2";
+#module_list+=" " ; module_list+="e3-mrfioc2";
+
+
+case "$1" in
+    all)
+	setup_base_require
+	build_base_require
+	setup_env
+	setup_modules
+	build_modules
+	;;
+    base)
+    	setup_base_require
+	build_base_require
+	;;
+    modules)
+	setup_env
+	setup_modules
+	build_modules
+	;;
+    env)
+	print_list "${module_list[@]}"
+	;;
+    *)
+	help
+	;;
+esac
+
+exit 0;
+
+
+
+
 
