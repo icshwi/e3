5f7b5d8fc160396aa320cd791051afb4ddd6bee3
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-04-23 01:39:36 +0200

    help to make to clean e3-*:

--- .jenkin.sh
+++ .jenkin.sh
@@ -1,7 +1,14 @@
 
+echo ">>> Linux"
 uname -ar
 
+echo ">>> rm -rf e3-*"
 rm -rf e3-*
 
+
+echo ">>> Creating CONFIG_BASE.local ... "
+
 echo "E3_EPICS_PATH:=/tmp" > CONFIG_BASE.local
+
+echo ">>> Creating RELEASE.local ... "
 echo "EPICS_BASE:=/tmp/base-3.15.5" > RELEASE.local
