316ada3576b27c34197b0f6cc48613b16b882bfd
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-05-10 22:17:43 +0200

    add llrf building rules for jenkin

---
+++ .jenkin_llrf.sh
@@ -0,0 +1,47 @@
+
+echo ">>> Linux"
+uname -ar
+
+echo ">>> rm -rf e3-*"
+rm -rf e3-*
+
+
+TARGET=/tmp/llrf
+
+echo ">>> Creating CONFIG_BASE.local ... "
+
+echo "E3_EPICS_PATH:=${TARGET}" > CONFIG_BASE.local
+
+echo ">>> Creating RELEASE.local ... "
+echo "EPICS_BASE:=${TARGET}/base-3.15.5" > RELEASE.local
+
+
+git config --global url."git@bitbucket.org:".insteadOf https://bitbucket.org/
+git config --global url."git@gitlab.esss.lu.se:".insteadOf https://gitlab.esss.lu.se/
+
+bash e3.bash base
+bash e3.bash req
+
+
+bash e3.bash -cl cmod
+bash e3.bash -cl gmod
+
+bash e3.bash -c imod
+bash e3.bash -c bmod
+
+
+cd e3-asyn
+
+echo ">>> Creating CONFIG_MODULE.local for ASYN ... "
+
+echo "EPICS_MODULE_TAG:=tags/R4-27"  > configure/CONFIG_MODULE.local
+echo "E3_MODULE_VERSION:=4.27.0"    >> configure/CONFIG_MODULE.local
+
+make init
+make rebuild
+
+cd ..
+
+
+bash e3.bash -l imod
+bash e3.bash -l bmod
