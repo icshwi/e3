b0e773f696c9cf0ad798a4f06f532c8bf8deb4ae
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-05-10 14:29:33 +0200

    use ssh key instead of login, in jenkin

--- .jenkin.sh
+++ .jenkin.sh
@@ -12,3 +12,7 @@ echo "E3_EPICS_PATH:=/tmp" > CONFIG_BASE.local
 
 echo ">>> Creating RELEASE.local ... "
 echo "EPICS_BASE:=/tmp/base-3.15.5" > RELEASE.local
+
+
+git config --global url."git@bitbucket.org:".insteadOf https://bitbucket.org/
+git config --global url."git@gitlab.esss.lu.se:".insteadOf https://gitlab.esss.lu.se/
