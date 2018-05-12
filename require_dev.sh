68281fcfa29eb27cb532a31f9b6d554922cda0ea
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-05-12 21:38:34 +0200

    add require dev script

---
+++ require_dev.sh
@@ -0,0 +1,27 @@
+
+echo ">>> Linux"
+uname -ar
+
+echo ">>> rm -rf e3-*"
+rm -rf e3-*
+
+TARGET="/epics/devreq"
+
+echo ">>> Creating CONFIG_BASE.local ... "
+
+echo "E3_EPICS_PATH:=${TARGET}" > CONFIG_BASE.local
+
+echo ">>> Creating RELEASE.local ... "
+echo "EPICS_BASE:=${TARGET}" > RELEASE.local
+echo "E3_REQUIRE_VERSION:=0.0.1" >> RELEASE.local
+
+
+echo ">>> Creating RELEASE_DEV.local for require... "
+echo "EPICS_BASE:=${TARGET}/base-3.15.5" > RELEASE_DEV.local
+echo "E3_REQUIRE_VERSION:=0.0.1" >> RELEASE_DEV.local
+
+
+bash e3.bash base
+bash e3.bash devreq
+bash e3.bash -ctifea mod
+
