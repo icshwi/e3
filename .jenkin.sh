6ec37f41d1632142feb913e4af184353d06e9889
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-05-17 22:34:22 +0200

    update jenkins.sh due to dir permissions issue

--- .jenkin.sh
+++ .jenkin.sh
@@ -5,13 +5,17 @@ uname -ar
 echo ">>> rm -rf e3-*"
 rm -rf e3-*
 
+TARGET="/jenkins/epics"
+epics_base="${TARGET}/base-3.15.5"
 
+
+echo ""
 echo ">>> Creating CONFIG_BASE.local ... "
+echo "E3_EPICS_PATH:=${TARGET}" > CONFIG_BASE.local
 
-echo "E3_EPICS_PATH:=/tmp" > CONFIG_BASE.local
-
+echo ""
 echo ">>> Creating RELEASE.local ... "
-echo "EPICS_BASE:=/tmp/base-3.15.5" > RELEASE.local
+echo "EPICS_BASE:=${epics_base}" > RELEASE.local
 
 
 git config --global url."git@bitbucket.org:".insteadOf https://bitbucket.org/
