cd872ee07d927cd957541a0d4ecbc9f6edce2e4c
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-09-09 23:41:20 +0200

    echoing

--- .jenkin_epics7.sh
+++ .jenkin_epics7.sh
@@ -13,6 +13,11 @@ epics_base="${TARGET}/base-${VERSION}"
 
 echo ""
 echo ">>> Creating CONFIG_BASE.local ... "
+echo "E3_EPICS_PATH:=${TARGET}" 
+echo "EPICS_BASE_TAG:=tags/R${VERSION}"
+echo "E3_BASE_VERSION:=${VERSION}"     
+echo "E3_CROSS_COMPILER_TARGET_ARCHS ="
+
 echo "E3_EPICS_PATH:=${TARGET}"          > CONFIG_BASE.local
 echo "EPICS_BASE_TAG:=tags/R${VERSION}" >> CONFIG_BASE.local
 echo "E3_BASE_VERSION:=${VERSION}"      >> CONFIG_BASE.local
@@ -21,6 +26,10 @@ echo "E3_CROSS_COMPILER_TARGET_ARCHS =" >> CONFIG_BASE.local
 
 echo ""
 echo ">>> Creating RELEASE.local ... "
+echo "EPICS_BASE:=${epics_base}"  
+echo "E3_SEQUENCER_NAME:=seq"     
+echo "E3_SEQUENCER_VERSION:=2.2.6" 
+
 echo "EPICS_BASE:=${epics_base}"   > RELEASE.local
 echo "E3_SEQUENCER_NAME:=seq"      >> RELEASE.local
 echo "E3_SEQUENCER_VERSION:=2.2.6" >> RELEASE.local
