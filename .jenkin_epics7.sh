76363cdcd2ea714e1a70659073721d8dde46fa9c
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-09-09 23:37:36 +0200

    add seq version

--- .jenkin_epics7.sh
+++ .jenkin_epics7.sh
@@ -21,8 +21,9 @@ echo "E3_CROSS_COMPILER_TARGET_ARCHS =" >> CONFIG_BASE.local
 
 echo ""
 echo ">>> Creating RELEASE.local ... "
-echo "EPICS_BASE:=${epics_base}" > RELEASE.local
-
+echo "EPICS_BASE:=${epics_base}"   > RELEASE.local
+echo "E3_SEQUENCER_NAME:=seq"      >> RELEASE.local
+echo "E3_SEQUENCER_VERSION:=2.2.6" >> RELEASE.local
 
 git config --global url."git@bitbucket.org:".insteadOf https://bitbucket.org/
 git config --global url."git@gitlab.esss.lu.se:".insteadOf https://gitlab.esss.lu.se/
