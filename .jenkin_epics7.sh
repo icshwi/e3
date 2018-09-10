0d2432f1b1cce545ba23e550e585ada3fa484396
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-09-10 10:05:16 +0200

    changed sequencer name to sequencer from seq for 2.2

--- .jenkin_epics7.sh
+++ .jenkin_epics7.sh
@@ -27,12 +27,12 @@ echo "E3_CROSS_COMPILER_TARGET_ARCHS =" >> CONFIG_BASE.local
 echo ""
 echo ">>> Creating RELEASE.local ... "
 echo "EPICS_BASE:=${epics_base}"  
-echo "E3_SEQUENCER_NAME:=seq"     
+echo "E3_SEQUENCER_NAME:=sequencer"     
 echo "E3_SEQUENCER_VERSION:=2.2.6" 
 
-echo "EPICS_BASE:=${epics_base}"   > RELEASE.local
-echo "E3_SEQUENCER_NAME:=seq"      >> RELEASE.local
-echo "E3_SEQUENCER_VERSION:=2.2.6" >> RELEASE.local
+echo "EPICS_BASE:=${epics_base}"     > RELEASE.local
+echo "E3_SEQUENCER_NAME:=sequencer"  >> RELEASE.local
+echo "E3_SEQUENCER_VERSION:=2.2.6"   >> RELEASE.local
 
 git config --global url."git@bitbucket.org:".insteadOf https://bitbucket.org/
 git config --global url."git@gitlab.esss.lu.se:".insteadOf https://gitlab.esss.lu.se/
