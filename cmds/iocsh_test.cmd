f774aa1e9e2e7e4fd1a63aa0f9e963ae162bd01e
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-09-19 01:20:55 +0200

    one line for modules for as, iocStats, recsync

--- cmds/iocsh_test.cmd
+++ cmds/iocsh_test.cmd
@@ -1,10 +1,23 @@
 
-var requireDebug 1
-require iocStats,617a4fd
+require iocStats,ae5d083
+require recsync,1.3.0
+require autosave,5.9.0
+
+epicsEnvSet("SEC", "SEC")
+epicsEnvSet("SUB", "SUB01")
+epicsEnvSet("P", "$(SEC)-$(SUB):")
+epicsEnvSet("DIS", "DIS")
+epicsEnvSet("DEV", "DEV-01")
+epicsEnvSet("R", "$(DIS)-$(DEV)")
+epicsEnvSet("IOCNAME", "$(P)$(R)")
 
 
+# need to define IOCNAME
+# PV names
+loadIocsh("iocStats.iocsh", "IOCNAME=$(IOCNAME)")
+loadIocsh("recsync.iocsh",  "IOCNAME=$(IOCNAME)")
+loadIocsh("autosave.iocsh", "IOCNAME=$(IOCNAME), AS_TOP=/tmp")
 
-loadIocsh("iocStats.iocsh", "IOCNAME=TEST1:")
 
 iocInit()
 
