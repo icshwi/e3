ea2974a6c920f8419ea36e531ff9573db4b873f6
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-10-03 15:56:10 +0200

    tweak for screens

--- .jenkins/screen_iocsh_jenkins.bash
+++ .jenkins/screen_iocsh_jenkins.bash
@@ -5,12 +5,35 @@ declare -gr SC_SCRIPTNAME=${0##*/}
 declare -gr SC_TOP="${SC_SCRIPT%/*}"
 
 
-E3_PATH=$1
-BASE=$2
-REQUIRE=$3
+E3_PATH="$1"
+BASE="$2"
+REQUIRE="$3"
+
 
 SESSION="${BASE}-${REQUIRE}"
 
-. ${E3_PATH}/base-${BASE}/require/${REQUIRE}/bin/setE3Env.bash
+#source ${E3_PATH}/base-${BASE}/require/${REQUIRE}/bin/setE3Env.bash
 
-bash ${SC_TOP}/screen_jenkins.bash ${SESSION} ${SC_TOP}/../.cmd
+sudo chgrp root $(which screen)
+sudo chmod 777 /var/run/screen
+
+cat > ${E3_PATH}/.screenrc <<EOF
+
+logfile ${E3_PATH}/output.log
+logfile flush 1
+log on
+logtstamp after 1
+logtstamp on
+EOF
+
+
+screen -c ${E3_PATH}/.screenrc -dm -L -S ${SESSION} /bin/bash -c "source ${E3_PATH}/base-${BASE}/require/${REQUIRE}/bin/setE3Env.bash && iocsh.bash ${SC_TOP}/../.cmd"
+
+sleep 10
+
+cat ${E3_PATH}/output.log
+
+sleep 5
+
+killall screen
+
