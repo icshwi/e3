bd39f0e5a3f5d98fe9b8d78fe8e09189b5d021c9
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-10-03 12:11:20 +0200

    group them in a script in before_install

---
+++ .ci/before_install_conf.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p ${HOME}/.ssh
+echo -e "Host *\n StrictHostKeyChecking no" > ${HOME}/.ssh/config
+echo -e "machine bitbucket.org\n  login jeonghanlee\n  password $CI_USER_PASSWORD" > ${HOME}/.netrc
+
+bash .ci/ethercat.sh
+#  source .ci/ethercat_libpath.sh
