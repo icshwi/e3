5e376e9bc81f8f46a0565aa803e07cf18d956a8d
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-10-03 12:14:48 +0200

    suppress output in ethercat

--- .ci/before_install_conf.bash
+++ .ci/before_install_conf.bash
@@ -4,5 +4,5 @@ mkdir -p ${HOME}/.ssh
 echo -e "Host *\n StrictHostKeyChecking no" > ${HOME}/.ssh/config
 echo -e "machine bitbucket.org\n  login jeonghanlee\n  password $CI_USER_PASSWORD" > ${HOME}/.netrc
 
-bash .ci/ethercat.sh
+bash .ci/ethercat.sh  > /dev/null 2>&1 
 #  source .ci/ethercat_libpath.sh
