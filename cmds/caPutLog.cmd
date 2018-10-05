6c3f80f9055232f89104741443ee8b2c2ff0af36
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-10-05 17:37:25 +0200

    add caPutlog startup script [skip ci]

--- cmds/caPutLog.cmd
+++ cmds/caPutLog.cmd
@@ -1,3 +1,4 @@
+require ess,0.0.1
 require caPutLog,3.6.0
 require iocStats,ae5d083
 
@@ -8,14 +9,18 @@ epicsEnvSet("P", "$(SEC)-$(SUB):")
 epicsEnvSet("DIS", "DIS")
 epicsEnvSet("DEV", "DEV-01")
 epicsEnvSet("R", "$(DIS)-$(DEV)")
-epicsEnvSet("IOCNAME", "$(P)$(R)caPutLog")
+epicsEnvSet("IOCNAME", "$(P)$(R)")
 
-epicsEnvSet("TOP", "$(E3_CMD_TOP)/../")
-epicsEnvSet("CALOGPUT_TOP", "$(TOP)/e3-caPutLog/iocsh")
+epicsEnvSet("TOP",          "$(E3_CMD_TOP)/..")
+epicsEnvSet("CALOGPUT_TOP", "$(TOP)/e3-caPutLog")
+epicsEnvSet("ESS_TOP",      "$(TOP)/e3-ess")
 
-#loadIocsh("iocStats.iocsh", "IOCNAME=$(IOCNAME)")
 
-loadIocsh("$(CALOGPUT_TOP)/caPutLog.iocsh", "IOCNAME=$(IOCNAME), LOG_INET=10.0.6.172")
+
+loadIocsh("accessSecurityGroup.iocsh", "ASG_PATH=$(ESS_TOP)/template, ASG_FILE=unrestricted_access.asg")
+loadIocsh("iocStats.iocsh", "IOCNAME=$(P)")
+#loadIocsh("iocLog.iocsh", "IOCNAME=$(IOCNAME), LOG_INET=10.0.6.172")
+loadIocsh("$(CALOGPUT_TOP)/iocsh/caPutLog.iocsh", "IOCNAME=$(IOCNAME), LOG_INET=10.0.6.172")
 
 iocInit()
 
