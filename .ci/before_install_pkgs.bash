7afe6a293e7d9cb83b6915d224f40d7606c430c4
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-10-06 21:34:20 +0200

    diable output for the pkgs installtion [skip ci]

--- .ci/before_install_pkgs.bash
+++ .ci/before_install_pkgs.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get -qq update
-sudo apt-get install -y linux-headers-$(uname -r) 
-sudo apt-get install -y build-essential realpath ipmitool libtool automake tclx  tree screen re2c darcs
-sudo apt-get install -y libreadline-dev libxt-dev x11proto-print-dev libxmu-headers libxmu-dev libxpm-dev libxmuu-dev libxmuu1 libpcre++-dev libsnmp-dev python-dev libnetcdf-dev libhdf5-dev libpng-dev libbz2-dev libxml2-dev libusb-dev libusb-1.0-0-dev libudev-dev libraw1394-dev libboost-dev libboost-regex-dev libboost-filesystem-dev 
+sudo apt-get -qq update  > /dev/null 2>&1 
+sudo apt-get install -y linux-headers-$(uname -r)   > /dev/null 2>&1 
+sudo apt-get install -y build-essential realpath ipmitool libtool automake tclx  tree screen re2c darcs  > /dev/null 2>&1 
+sudo apt-get install -y libreadline-dev libxt-dev x11proto-print-dev libxmu-headers libxmu-dev libxpm-dev libxmuu-dev libxmuu1 libpcre++-dev libsnmp-dev python-dev libnetcdf-dev libhdf5-dev libpng-dev libbz2-dev libxml2-dev libusb-dev libusb-1.0-0-dev libudev-dev libraw1394-dev libboost-dev libboost-regex-dev libboost-filesystem-dev  > /dev/null 2>&1
+ 
