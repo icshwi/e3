2ffd75d044a91ea5597ffac11f118a29d14e6403
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-11-19 09:31:23 +0100

    update iocStats example

--- cmds/iocStats.cmd
+++ cmds/iocStats.cmd
@@ -1,16 +1,13 @@
 require iocStats,ae5d083
 
-
 epicsEnvSet("TOP", "$(E3_CMD_TOP)")
 
-system "tr -cd 0-9 </dev/urandom | head -c 8 > $(TOP)/random_tmp"
-system "C=`cat random_tmp` && /bin/sed -e "s:_random_:$C:g" < $(TOP)/random.in > $(TOP)/random.cmd"
+system "bash $(TOP)/random.bash"
 
-< $(TOP)/random.cmd
+iocshLoad "$(TOP)/random.cmd"
 
 epicsEnvSet("P", "IOC-$(NUM)")
 epicsEnvSet("IOCNAME", "$(P)")
-
 
 loadIocsh("iocStats.iocsh", "IOCNAME=$(IOCNAME)")
 
