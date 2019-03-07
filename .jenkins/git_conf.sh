5fcdcb65f01156de14db88cb3778823aec74d4f6
Jeong Han Lee <jeonghan.lee@gmail.com>
2019-03-07 23:49:01 +0100

    minor update for jenkins

--- .jenkins/git_conf.sh
+++ .jenkins/git_conf.sh
@@ -1,4 +1,4 @@
-
-git config --global url."git@bitbucket.org:".insteadOf https://bitbucket.org/
-git config --global url."git@gitlab.esss.lu.se:".insteadOf https://gitlab.esss.lu.se/
-
+if [ ! -f /var/lib/jenkins/.gitconfig ]; then
+    git config --global url."git@bitbucket.org:".insteadOf https://bitbucket.org/
+    git config --global url."git@gitlab.esss.lu.se:".insteadOf https://gitlab.esss.lu.se/
+fi
