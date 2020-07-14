974cc795d35ce4879b6fcfb2742a27b9198e0968
Timo Korhonen <timo.korhonen@esss.se>
2020-07-14 09:47:39 +0200

    Updated iocStats version
    Updated iocStats version so that the IOC runs when cloned from repo.

--- cmds/ioc1.cmd
+++ cmds/ioc1.cmd
@@ -2,7 +2,7 @@
 ## This is the example how to use $(E3_CMD_TOP)
 ##
 ## 
-require iocStats,1856ef5
+require iocStats,3.1.16
 
 epicsEnvSet(TOP, "$(E3_CMD_TOP)/..")
 epicsEnvSet(IOCSTATS_CMD_TOP, "$(TOP)/e3-iocStats/cmds")
